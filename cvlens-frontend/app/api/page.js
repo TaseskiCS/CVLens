3511251b72cffb6668829b34e56497c16a7733d1
michaelmarsillo <thecaptain49@hotmail.ca>
2025-02-08 20:11:35 -0500

    Updated page.js

--- cvlens-frontend/app/api/page.js
+++ cvlens-frontend/app/api/page.js
@@ -3,8 +3,30 @@ export default async function Page() {
  
   return (
     <>
-        <h1>API Documentation!!
-        </h1>
+        {/* Header Section */}
+      <header className="bg-gray-800 text-white py-4">
+        <nav className="container mx-auto flex justify-center">
+          <ul className="flex space-x-20">
+            <li><a href="" className="hover:text-blue-400">Home</a></li>
+            <li><a href="" className="hover:text-blue-400">API Info</a></li>
+            <li><a href="" className="hover:text-blue-400">Creators</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      {/* Title Section */}
+      <section className="bg-gray-50 py-16">
+      <div className="container mx-auto text-center px-4">
+      <h2 className="text-3xl font-semibold text-gray-900 mb-4">
+        Automatically parse your resume properly with our tool
+      </h2>
+      <p className="text-lg text-gray-600">
+      </p>
+    </div>
+  </section>
+
+
+
     </>
   )
 }
